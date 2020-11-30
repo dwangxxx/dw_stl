12cdbd150f5ad9378a7849bd4801b9848323a5cb
SundyTang <dwang_slam@163.com>
2020-11-30 11:25:11 +0800

    完成了algorithm.h文件

--- dw_stl/algorithm.h
+++ dw_stl/algorithm.h
@@ -63,7 +63,7 @@ namespace dw_stl
     // 将[first, last)区间的元素拷贝到以result开头空间内
     /*******************************************************************/
 
-    // unchecked_copy_cat的input_iterator_tag版本
+    // copy_cat_dispatch的input_iterator_tag版本
     template <class InputIter, class OutputIter>
     OutputIter copy_cat_dispatch(InputIter first, InputIter last, OutputIter result, dw_stl::input_iterator_tag)
     {
@@ -72,7 +72,7 @@ namespace dw_stl
         return result;
     }
 
-    // unchecked_copy_cat的random_access_iterator_tag版本
+    // copy_cat_dispatch的random_access_iterator_tag版本
     template <class RandomIter, class OuputIter>
     OuputIter copy_cat_dispatch(RandomIter first, RandomIter last, OuputIter result, dw_stl::random_access_iterator_tag)
     {
@@ -117,7 +117,7 @@ namespace dw_stl
     // 将 [first, last)区间内的元素拷贝到 [result - (last - first), result)内
     /*******************************************************************/
 
-    // unchecked_copy_backward_cat的bidirectional_iterator_tag版本(命名规则为其能提供的最小功能)
+    // copy_backward_cat_dispatch的bidirectional_iterator_tag版本(命名规则为其能提供的最小功能)
     template <class BidirecitonalIter1, class BidirectionalIter2>
     BidirectionalIter2 copy_backward_cat_dispatch(BidirecitonalIter1 first, BidirecitonalIter1 last,
                                                    BidirectionalIter2 result, dw_stl::bidirectional_iterator_tag)
@@ -127,7 +127,7 @@ namespace dw_stl
         return result;
     }
 
-    // unchecked_copy_backward_cat的random_access_iterator_tag版本(命名规则为其能提供的最小功能)
+    // copy_backward_cat_dispatch的random_access_iterator_tag版本(命名规则为其能提供的最小功能)
     template <class BidirectionalIter1, class BidirectionalIter2>
     BidirectionalIter2 copy_backward_cat_dispatch(BidirectionalIter1 first, BidirectionalIter1 last,
                                                    BidirectionalIter2 result, dw_stl::random_access_iterator_tag)
@@ -174,7 +174,7 @@ namespace dw_stl
     // 将 [first, first + n)区间内的元素拷贝到 [result, result + n)上
     // 返回一个pair分别指向拷贝的尾部(first + n, result + n)
     /*******************************************************************/
-    // unchecked_copy_n的input_iterator_tag版本
+    // copy_n_dispatch的input_iterator_tag版本
     template <class InputIter, class Size, class OutputIter>
     dw_stl::pair<InputIter, OutputIter> copy_n_dispatch(InputIter first, Size n, OutputIter result, dw_stl::input_iterator_tag)
     {
@@ -184,7 +184,7 @@ namespace dw_stl
         return dw_stl::pair<InputIter, OutputIter>(first, result);
     }
 
-    // unchecked_copy_n的random_access_iterator_tag版本
+    // copy_n_dispatch的random_access_iterator_tag版本
     template <class RandomIter, class Size, class OutputIter>
     dw_stl::pair<RandomIter, OutputIter> copy_n_dispatch(RandomIter first, Size n, OutputIter result, dw_stl::random_access_iterator_tag)
     {
@@ -204,7 +204,7 @@ namespace dw_stl
     // move函数
     // 将 [first, last)区间内的元素移动到 [result, result + (last - first))上
     /*******************************************************************/
-    // unchecked_move_cat的input_iterator_tag版本
+    // move_cat_dispatch的input_iterator_tag版本
     template <class InputIter, class OutputIter>
     OutputIter move_cat_dispatch(InputIter first, OutputIter last, OutputIter result,
                                   dw_stl::input_iterator_tag)
@@ -214,7 +214,7 @@ namespace dw_stl
         return result;
     }
 
-    // unchecked_move_cat的random_access_iterator_tag版本
+    // move_cat_dispatch的random_access_iterator_tag版本
     template <class RandomIter, class OutputIter>
     OutputIter move_cat_dispatch(RandomIter first, RandomIter last, OutputIter result,
                                   dw_stl::random_access_iterator_tag)
@@ -256,7 +256,230 @@ namespace dw_stl
     // move_backward函数
     // 将 [first, last)区间内的元素移动到 [result - (last - first), result)上
     /*******************************************************************/
-    // 
+    // move_backward_cat_dispatch的bidirectional_iterator_tag版本
+    template <class BidirectionalIter1, class BidirectionalIter2>
+    BidirectionalIter2 move_backward_cat_dispatch(BidirectionalIter1 first, BidirectionalIter1 last,
+                                                  BidirectionalIter2 result, dw_stl::bidirectional_iterator_tag)
+    {
+        while (first != last)
+            *--result = dw_stl::move(*last);
+        return result;
+    }
+
+    // move_backward_cat_dispatch的random_access_iterator_tag版本
+    template <class RandomIter1, class RandomIter2>
+    RandomIter2 move_backward_cat_dispatch(RandomIter1 first, RandomIter1 last,
+                                           RandomIter2 result, dw_stl::random_access_iterator_tag)
+    {
+        for (auto n = last - first; n > 0; --n)
+            *--result = dw_stl::move(*--last);
+        return result;
+    }
+
+    // move_backward_dispatch的迭代器版本(自定义类型)
+    template <class BidirecitonalIter1, class BidirectionalIter2>
+    BidirectionalIter2 move_backward_dispatch(BidirecitonalIter1 first, BidirecitonalIter1 last,
+                                              BidirectionalIter2 result)
+    {
+        return move_backward_cat_dispatch(first, last, result, iterator_category(first));
+    }
+
+    // move_backward_dispatch的原生类型版本(特化版本)
+    template <class Tp, class Up>
+    typename std::enable_if<
+        std::is_same<typename std::remove_const<Tp>::type, Up>::value &&
+        std::is_trivially_move_assignable<Up>::value, Up*>::type
+    move_backward_dispatch(Tp* first, Tp* last, Up* result)
+    {
+        const size_t n = static_cast<size_t>(last - first);
+        if (n != 0)
+        {
+            result -= n;
+            // 直接调用标准库的memmove函数
+            std::memmove(result, first, n * sizeof(Up));
+        }
+        return result;
+    }
+
+    // move_backward接口函数
+    template <class BidirectionalIter1, class BidirectionalIter2>
+    BidirectionalIter2 move_backward(BidirectionalIter1 first, BidirectionalIter1 last,
+                                     BidirectionalIter2 result)
+    {
+        return move_backward_dispatch(first, last, result);
+    }
+
+    /*******************************************************************/
+    // equal比较函数
+    // 比较[first1, last1)序列上的元素是否和first2开头的元素相等
+    /*******************************************************************/
+    // 函数对象：函数名、函数指针、重载了()运算符的类
+    template <class InputIter1, class InputIter2>
+    bool equal(InputIter1 first1, InputIter1 last1, InputIter2 first2)
+    {
+        for (; first1 != last1; ++first1, ++first2)
+        {
+            if (*first1 != *first2)
+                return false;
+        }
+        return true;
+    }
+    
+    // 重载版本使用函数对象 comp 代替比较操作, 可以传入函数指针、函数名、重载了()运算符的对象
+    template <class InputIter1, class InputIter2, class Compare>
+    bool equal(InputIter1 first1, InputIter2 last1, InputIter2 first2, Compare cmp)
+    {
+        for (; first1 != last1; ++first1, ++first2)
+        {
+            if (!cmp(*first1, *first2))
+                return false;
+        }
+
+        return true;
+    }
+
+    /*******************************************************************/
+    // fill_n
+    // 从first位置开始填充n个值
+    /*******************************************************************/
+    template <class OuputIter, class Size, class T>
+    OuputIter fill_n_dispatch(OuputIter first, Size n, const T& value)
+    {
+        for (; n > 0; --n, ++first)
+            *first = value;
+        return first;
+    }
+
+    // 为one-byte提供特化版本
+    template <class Tp, class Size, class Up>
+    typename std::enable_if<
+        std::is_integral<Tp>::value && sizeof(Tp) == 1 &&
+        !std::is_same<Tp, bool>::value &&
+        std::is_integral<Up>::value && sizeof(Up) == 1, Tp*>::type
+    fill_n_dispatch(Tp* first, Size n, Up value)
+    {
+        if (n > 0)
+            std::memset(first, (unsigned char)value, (size_t)(n));
+        return first + n;
+    }
+
+    template <class OuputIter, class Size, class T>
+    OuputIter fill_n(OuputIter first, Size n, const T& value)
+    {
+        return fill_n_dispatch(first, n, value);
+    }
+
+    /*******************************************************************/
+    // fill
+    // 为[first, last)区间捏的所有元素填充新值
+    /*******************************************************************/
+    // forward_iterator_tag版本(因为bidirectional是继承自forward的，因此bidirectional_iterator也可以调用)
+    template <class ForwardIter, class T>
+    void fill_dispatch(ForwardIter first, ForwardIter last, const T& value,
+                    dw_stl::forward_iterator_tag)
+    {
+        for (; first != last; ++first)
+            *first = value;
+    }
+
+    // random_access_iterator_tag版本
+    template<class RandomIter, class T>
+    void fill_dispatch(RandomIter first, RandomIter last, const T& value,
+                    dw_stl::random_access_iterator_tag)
+    {
+        // 直接调用fill_n函数
+        fill_n(first, last - first, value);
+    }
+
+    // fill函数接口
+    template <class ForwardIter, class T>
+    void fill(ForwardIter first, ForwardIter last, const T& value)
+    {
+        fill_dispatch(first, last, value, iterator_category(first));
+    }
+
+    /*******************************************************************/
+    // lexicographical_compare
+    // 以字典序排列对两个序列进行比较，当在某个位置发现第一组不相等元素时，有下列几种情况：
+    // (1)如果第一序列的元素较小，返回 true ，否则返回 false
+    // (2)如果到达 last1 而尚未到达 last2 返回 true
+    // (3)如果到达 last2 而尚未到达 last1 返回 false
+    // (4)如果同时到达 last1 和 last2 返回 false
+    /*******************************************************************/
+    template <class InputIter1, class InputIter2>
+    bool lexicographical_compare(InputIter1 first1, InputIter1 last1,
+                                 InputIter2 first2, InputIter2 last2)
+    {
+        for (; first1 != last1 && first2 != last2; ++first1, ++first2)
+        {
+            if (*first1 < *first2)
+                return true;
+            if (*first2 < *first1)
+                return false;
+        }
+
+        return first1 == last1 && first2 != last2;
+    }
+
+    // 重载比较对象comp版本
+    template <class InputIter1, class InputIter2, class Compare>
+    bool lexicographical_compare(InputIter1 first1, InputIter1 last1,
+                                 InputIter2 first2, InputIter2 last2, Compare cmp)
+    {
+        for (; first1 != last1 && first2 != last2; ++first1, ++first2)
+        {
+            if (cmp(*first1, *first2))
+                return true;
+            if (cmp(*first2, *first1))
+                return false;
+        }
+
+        return first1 == last1 && first2 != last2;
+    }
+
+    // 针对const unsigned char*的特化版本
+    bool lexicographical_compare(const unsigned char* first1,
+                                 const unsigned char* last1,
+                                 const unsigned char* first2,
+                                 const unsigned char* last2)
+    {
+        const auto len1 = last1 - first1;
+        const auto len2 = last2 - first2;
+        // 先比较相同长度
+        // memcmp比较，如果count长度超过字符串长度，则继续比较，不会在'/0'处停下
+        const auto result = std::memcmp(first1, first2, dw_stl::min(len1, len2));
+        // 如果相等，长度较长的比较大
+        return result != 0 ? result < 0 : len1 < len2;
+    }
+
+    /*****************************************************************************************/
+    // mismatch
+    // 平行比较两个序列，找到第一处失配的元素，返回一对迭代器，分别指向两个序列中失配的元素
+    /*****************************************************************************************/
+    template <class InputIter1, class InputIter2>
+    dw_stl::pair<InputIter1, InputIter2> mismatch(InputIter1 first1, InputIter1 last1, InputIter2 first2)
+    {
+        while (first1 != last1 && *first1 == *first2)
+        {
+            ++first1;
+            ++first2;
+        }
+
+        return dw_stl::pair<InputIter1, InputIter2>(first1, first2);
+    }
+
+    // 重载cmp的mismatch版本
+    template <class InputIter1, class InputIter2, class Compare>
+    dw_stl::pair<InputIter1, InputIter2> mismatch(InputIter1 first1, InputIter1 last,
+                                                  InputIter2 first2, Compare cmp)
+    {
+        while (first1 != last1 && cmp(*first1, *first2))
+        {
+            ++first1;
+            ++first2;
+        }
+        return dw_stl::pair<InputIter1, InputIter2>(first1, first2);
+    }
 }
 
 #endif
